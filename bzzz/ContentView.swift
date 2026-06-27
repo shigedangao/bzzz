812263491b46aaad3cea6666af5cbfb9dbcd30b9
shigedangao <marc.inthaamnouay@gmail.com>
2026-06-27 21:50:35 +0200

    Initial Commit

---
+++ bzzz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bzzz
+//
+//  Created by Marc Intha on 27/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bzzz/bzzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  bzzzApp.swift
+//  bzzz
+//
+//  Created by Marc Intha on 27/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct bzzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
